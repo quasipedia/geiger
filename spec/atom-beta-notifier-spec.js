03fc62e728d8e61ae2f5c483cdbf2c340a09feab
Mac Ryan <quasipedia@gmail.com>
2016-08-03 22:19:24 +0200

    Code cleanup /3

--- spec/atom-beta-notifier-spec.js
+++ spec/atom-beta-notifier-spec.js
@@ -15,59 +15,34 @@ describe('AtomBetaNotifier', () => {
     activationPromise = atom.packages.activatePackage('atom-beta-notifier');
   });
 
-  describe('when the atom-beta-notifier:toggle event is triggered', () => {
-    it('hides and shows the modal panel', () => {
-      // Before the activation event the view is not on the DOM, and no panel
-      // has been created
-      expect(workspaceElement.querySelector('.atom-beta-notifier')).not.toExist();
+  describe('when the atom-beta-notifier:check event is triggered', () => {
 
-      // This is an activation event, triggering it will cause the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-beta-notifier:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        expect(workspaceElement.querySelector('.atom-beta-notifier')).toExist();
-
-        let atomBetaNotifierElement = workspaceElement.querySelector('.atom-beta-notifier');
-        expect(atomBetaNotifierElement).toExist();
-
-        let atomBetaNotifierPanel = atom.workspace.panelForItem(atomBetaNotifierElement);
-        expect(atomBetaNotifierPanel.isVisible()).toBe(true);
-        atom.commands.dispatch(workspaceElement, 'atom-beta-notifier:toggle');
-        expect(atomBetaNotifierPanel.isVisible()).toBe(false);
-      });
-    });
-
-    it('hides and shows the view', () => {
-      // This test shows you an integration test testing at the view level.
-
-      // Attaching the workspaceElement to the DOM is required to allow the
-      // `toBeVisible()` matchers to work. Anything testing visibility or focus
-      // requires that the workspaceElement is on the DOM. Tests that attach the
-      // workspaceElement to the DOM are generally slower than those off DOM.
-      jasmine.attachToDOM(workspaceElement);
-
-      expect(workspaceElement.querySelector('.atom-beta-notifier')).not.toExist();
-
-      // This is an activation event, triggering it causes the package to be
-      // activated.
-      atom.commands.dispatch(workspaceElement, 'atom-beta-notifier:toggle');
-
-      waitsForPromise(() => {
-        return activationPromise;
-      });
-
-      runs(() => {
-        // Now we can test for view visibility
-        let atomBetaNotifierElement = workspaceElement.querySelector('.atom-beta-notifier');
-        expect(atomBetaNotifierElement).toBeVisible();
-        atom.commands.dispatch(workspaceElement, 'atom-beta-notifier:toggle');
-        expect(atomBetaNotifierElement).not.toBeVisible();
-      });
-    });
+    // it('hides and shows the view', () => {
+    //   // This test shows you an integration test testing at the view level.
+    //
+    //   // Attaching the workspaceElement to the DOM is required to allow the
+    //   // `toBeVisible()` matchers to work. Anything testing visibility or focus
+    //   // requires that the workspaceElement is on the DOM. Tests that attach the
+    //   // workspaceElement to the DOM are generally slower than those off DOM.
+    //   jasmine.attachToDOM(workspaceElement);
+    //
+    //   expect(workspaceElement.querySelector('.atom-beta-notifier')).not.toExist();
+    //
+    //   // This is an activation event, triggering it causes the package to be
+    //   // activated.
+    //   atom.commands.dispatch(workspaceElement, 'atom-beta-notifier:check');
+    //
+    //   waitsForPromise(() => {
+    //     return activationPromise;
+    //   });
+    //
+    //   runs(() => {
+    //     // Now we can test for view visibility
+    //     let atomBetaNotifierElement = workspaceElement.querySelector('.atom-beta-notifier');
+    //     expect(atomBetaNotifierElement).toBeVisible();
+    //     atom.commands.dispatch(workspaceElement, 'atom-beta-notifier:toggle');
+    //     expect(atomBetaNotifierElement).not.toBeVisible();
+    //   });
+    // });
   });
 });
